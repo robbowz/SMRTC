684e4e60d2d9e6a70543a84ac719503f1df5eb9a
Robert <36878317+robbowz@users.noreply.github.com>
2018-09-16 07:08:48 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,13 +1,12 @@
-function download_wallet() {
 	echo "Downloading wallet..."
 	mkdir /root/smrtc
-  cd ucc
+  	cd smrtc
 	mkdir /root/.smrtc
 	wget https://github.com/telostia/smartcloud-guides/releases/download/0.001/smrtc-linux.tar.gz
 	tar -xvf smrtc-linux.tar.gz
 	rm /root/ucc/smrtc-linux.tar.gz
-	cp smrtc-linux.tar.gz/smrtcd /root/smrtc/smrtcd
-	cp smrtc-linux.tar.gz/smrtc-cli /root/smrtc/smrtc-cli
+	cp smrtcd /root/smrtc/smrtcd
+	cp smrtc-cli /root/smrtc/smrtc-cli
 	rm -rf smrtc-linux.tar.gz/
 	rm -rf /root/smrtc/smrtc-linux.tar.gz/
 	chmod +x /root/smrtc/
